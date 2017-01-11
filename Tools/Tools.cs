2d3faa11914e5df1f48145b268a3c92380795392
lars <guumholm34@gmail.com>
2017-01-11 17:58:10 +0100

    Adderet nyt direktorie Tools og ny fil : Tools\Tools.cs

---
+++ Tools/Tools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_GitHub.Tools
+{
+    public class Tools
+    {
+        public static int Add_Numbers(int Number1, int Number2)
+        {
+            return (Number1 + Number2);
+        }
+    }
+}
